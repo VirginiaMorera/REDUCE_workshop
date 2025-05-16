fcc8040b57f5085ff381c09693e45339aa30aed2
Virginia Morera-Pujol <morera.virginia@gmail.com>
2025-05-16 14:10:37 +0200

    Add partial response plots and code for binary map

--- Scripts/5_BART.R
+++ Scripts/5_BART.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(sf)
 library(terra)
 library(predicts)
-
+library(tidyterra)
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
 # 1. Load data ####
@@ -33,6 +33,7 @@ ggplot() +
   geom_spatraster(data = covars, aes(fill = bati)) +
   geom_sf(data = map, fill = "lightgray", col = "darkgray") + 
   geom_sf(data = ven_sf) + 
+  scale_fill_viridis_c() +
   coord_sf(xlim = ext(covars)[1:2], ylim = ext(covars)[3:4]) +
   theme_bw()
 
@@ -52,6 +53,7 @@ ggplot() +
   geom_sf(data = map, fill = "lightgray", col = "darkgray") + 
   geom_sf(data = absences_sf, col = "orange") + 
   geom_sf(data = ven_sf) + 
+  scale_fill_viridis_c() +
   coord_sf(xlim = ext(covars)[1:2], ylim = ext(covars)[3:4]) +
   theme_bw()
 
@@ -85,7 +87,6 @@ xvars <- names(model_df)[c(3,4,5)]
 sdm <-bart.step(x.data = model_df[,xvars],
                 y.data = model_df[,"pres"],
                 full = TRUE,
-                quiet = F, 
                 iter.step = 10, 
                 iter.plot = 10, 
                 tree.step = 2)
@@ -107,60 +108,27 @@ plot(prediction)
 
 #' We can now generate a plot of the importance of the included variables
 
-varimp_list <- list()
-for(i in seq_along(model_list)) {
-  # i = 1
-  
-  varimp <- varimp(model_list[[i]], plots = F)
-  
-  varimp_ordered <- varimp %>% 
-    arrange(-varimps) %>% 
-    mutate(iteration = names(model_list)[i])
-  
-  varimp_list[i] <- list(varimp_ordered)
-}
-
-varimp_pops <- map_df(varimp_list, bind_rows)
-row.names(varimp_pops) <- NULL
-
-varimp_pops <- varimp_pops %>% 
-  dplyr::select(iteration, Variable = names, Importance = varimps) %>% 
-  mutate(Variable = recode(Variable, sst = "Sea Surface Temperature", chla_var = "Chlorophyll A Variability", 
-                           logchla_lag3 = "Chlorophyll A", bati = "Bathimetry", 
-                           sst_grad = "Sea Surface Temperature gradient", sal = "Salinity", 
-                           slope = "Slope2"))
-
-write.csv(varimp_pops, 
-            file = "embarcadero_SDMs/final_outputs/colony_models/colony_varimp.csv")
-  
-
-varimp_pops %>% 
-  group_by(Variable) %>% 
-  summarise(mean = mean(Importance), 
-            sd = sd(Importance)) %>% 
-  ggplot() + 
-  geom_point(aes(x = Variable, y = mean)) + 
-  geom_errorbar(aes(x = Variable, ymin = mean-sd, ymax = mean+sd), width=.2) + 
-  theme_bw()
+varimp <- varimp(sdm, plots = T)
 
 
 ## Partial response ####
-#' These plots show the mean effect of each variable when measured at all other values of the other predictors, for all the values of the variable of interest. This will show us at which values of the covariate the dependence is higher.
-#' With CI you can add credible intervals, and with trace = TRUE you can see the dependence plot for each of the trees (remember BARTs are regression trees), with the thicker line representing the mean of all the trees. 
 
-for(i in seq_along(model_list)) {
-  # i = 1
-  sdm <- model_list[[i]]
-  
-  tmp_partial <- partial(sdm,
+partial_plots <- partial(sdm,
                          trace = FALSE,
                          ci = TRUE,
                          equal = TRUE,
                          smooth = 10, 
                          panels = F)
-  
-  saveRDS(tmp_partial, 
-          file = paste0("embarcadero_SDMs/outputs/partial_plots_", names(model_list)[i], ".RDS"))
-}
 
+partial_plots[[1]] + partial_plots[[2]] + 
+  partial_plots[[3]] + 
+  plot_layout(ncol = 2)
+saveRDS(partial_plots, file = "Data/Partial_plots.RDS")
 
+p <- pd2bart(sdm,
+             xind = c("bati", "sst"),
+             pl = TRUE)
+
+sp.dep.sst <- spartial(sdm, stack(covars), x.vars = 'sst', equal = TRUE)
+
+plot(rast(sp.dep.sst))
